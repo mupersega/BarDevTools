2e68d27c1b742c22a61d5ce5380ba7d08a952b9d
Cameron Taylor <cameron@vivedus.com>
2025-02-18 09:52:09 +1000

    updated vite config, rebuild

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/your-repo-name/', // Change this to your GitHub repository name
+  base: '/BarDevTools/', // Change this to your GitHub repository name
   build: {
     outDir: 'docs', // Ensure the output folder matches your GitHub Pages settings
   },
